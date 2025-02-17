3aad46bd9e120f01ec13febba36169be8e60c2ad
Victor Magueta <56882461+vmagueta@users.noreply.github.com>
2025-02-17 18:21:46 +0000

    Created Dockerfile and the endpoit of blog (#6)

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('blog/', views.blog, name="blog"),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
